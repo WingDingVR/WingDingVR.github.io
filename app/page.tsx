4ca7997fe192ff5936dff6bca57b658cb680a8d1
Marcus Sanatan <msanatan@gmail.com>
2024-08-28 15:48:02 -0400

    Improve text on website + add a better logo for the

--- app/page.tsx
+++ app/page.tsx
@@ -1,36 +1,40 @@
 "use client";
 import { ExternalLinkIcon } from "@chakra-ui/icons";
 import {
-  Box,
   Button,
   Center,
   Flex,
+  Image,
   SimpleGrid,
   Text,
   VStack,
 } from "@chakra-ui/react";
-import bgImage from "../public/wingding-bg.jpg";
 import Link from "next/link";
+
+import bgImage from "../public/wingding-bg.jpg";
 
 export default function Home() {
   return (
     <>
-      <Flex as="header" minH="40px" w="full" position="static" top={0}>
-        <Box>
-          <Text
-            fontFamily="mulish"
-            fontSize={["3xl", "6xl"]}
-            fontWeight={1000}
-            fontStyle="italic"
-          >
-            WingDing
-          </Text>
-        </Box>
+      <Flex
+        as="header"
+        w="full"
+        position="static"
+        top={0}
+        backgroundColor="black"
+      >
+        <Center w="full">
+          <Image
+            src="/wingding-logo.png"
+            alt="WingDing Logo"
+            boxSize={["128px", "256px", "320px"]}
+          />
+        </Center>
       </Flex>
       <Flex
         as="main"
         flex={1}
-        backgroundImage={`linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(${bgImage.src})`}
+        backgroundImage={`linear-gradient(rgba(0, 0, 0, 0.95), rgba(0, 0, 0, 0.5)), url(${bgImage.src})`}
         backgroundSize="cover"
         backgroundPosition="center"
       >
@@ -42,15 +46,19 @@ export default function Home() {
           paddingX={[4, 8, null]}
           spacing={[10, null]}
         >
-          <Center padding={[0, 2, 4]}>
+          <Center padding={[2, 4, 8]}>
             <VStack spacing={[2, 4]}>
               <Text fontFamily="roboto" fontSize={[18, 24, 32]} color="white">
-                We&apos;re crafting WingDing with a passionate community in
-                mind, and we need your help to bring this vision to life.
-                Support our journey and unlock exclusive rewards by backing our
-                Kickstarter campaign. Together, we can make WingDing the premier
-                destination for VR adventurers seeking the ultimate flight
-                experience.
+                We&apos;re crafting WingDing, the best place to take flight in
+                VR! WingDing gives you a space to fly across the world. Whether
+                you want race against the others to be the number one bird, or
+                perch with your fellow players, this is where you want to be!
+              </Text>
+              <Text fontFamily="roboto" fontSize={[18, 24, 32]} color="white">
+                We need your help to bring this vision to life! Support our
+                journey and unlock exclusive rewards by backing our Kickstarter
+                campaign. Together, we can make WingDing the premier destination
+                for VR adventurers seeking the ultimate flight experience.
               </Text>
               <Link
                 href="https://www.kickstarter.com/projects/wingding/wingding-vr-social-play-and-flight-experience/"
