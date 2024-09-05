4ac6118ee98007da54e326ca6cf3972921bf35f8
Marcus Sanatan <msanatan@gmail.com>
2024-09-04 20:53:47 -0400

    Simply page and use new logo

--- app/page.tsx
+++ app/page.tsx
@@ -11,54 +11,36 @@ import {
 } from "@chakra-ui/react";
 import Link from "next/link";
 
-import bgImage from "../public/wingding-bg.jpg";
+import bgImage from "../public/images/wingding-bg.jpg";
+import logoImage from "../public/images/logo.jpg";
 
 export default function Home() {
   return (
     <>
-      <Flex
-        as="header"
-        w="full"
-        position="static"
-        top={0}
-        backgroundColor="black"
-      >
-        <Center w="full">
-          <Image
-            src="/wingding-logo.png"
-            alt="WingDing Logo"
-            w={[256, 384, 768]}
-          />
-        </Center>
-      </Flex>
       <Flex
         as="main"
         flex={1}
         backgroundImage={`linear-gradient(rgba(0, 0, 0, 0.95), rgba(0, 0, 0, 0.95)), url(${bgImage.src})`}
         backgroundSize="cover"
         backgroundPosition="center"
+        padding={[2, 4, 8]}
       >
-        <SimpleGrid
-          columns={[1, 2]}
-          w="full"
-          minH="100%"
-          marginY={[4, 8, 16]}
-          paddingX={[4, 8, null]}
-          spacing={[10, null]}
-        >
+        <VStack spacing={[2, 4]}>
+          <Center padding={[0, 2, 4]}>
+            <Image src={logoImage.src} />
+          </Center>
           <Center padding={[2, 4, 8]}>
             <VStack spacing={[2, 4]}>
               <Text fontFamily="roboto" fontSize={[18, 24, 32]} color="white">
-                We&apos;re crafting WingDing, the best place to take flight in
-                VR! WingDing gives you a space to fly across the world. Whether
-                you want race against the others to be the number one bird, or
-                perch with your fellow players, this is where you want to be!
+                We&apos;re crafting WingDing, the best place to take flight in VR!
+                Whether you want race against the others to be the number one bird,
+                or perch with your fellow players, this is where you want to be.
               </Text>
               <Text fontFamily="roboto" fontSize={[18, 24, 32]} color="white">
                 We need your help to bring this vision to life! Support our
                 journey and unlock exclusive rewards by backing our Kickstarter
-                campaign. Together, we can make WingDing the premier destination
-                for VR adventurers seeking the ultimate flight experience.
+                campaign. Together, we can make WingDing the most fun flying
+                experience in VR!
               </Text>
               <Link
                 href="https://www.kickstarter.com/projects/wingding/wingding-vr-social-play-and-flight-experience/"
@@ -69,24 +51,12 @@ export default function Home() {
                   size={["sm", "md", "lg"]}
                   colorScheme="teal"
                 >
-                  Kickstarter
+                  Back the Kickstarter
                 </Button>
               </Link>
             </VStack>
           </Center>
-          <Center padding={[0, 2, 4]}>
-            <iframe
-              width="640"
-              height="360"
-              src="https://www.youtube.com/embed/9FoBV1h--8k?si=H4SsU66yCLFeb7kB"
-              title="YouTube video player"
-              style={{ border: "0" }}
-              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-              referrerPolicy="strict-origin-when-cross-origin"
-              allowFullScreen
-            ></iframe>
-          </Center>
-        </SimpleGrid>
+        </VStack>
       </Flex>
     </>
   );
